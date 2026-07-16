572f32448067d78353cfcd4f8c0fc5ae8884468b
Bengaile <gaile@gbipartnersllc.com>
2026-07-16 18:15:19 -0400

    Menu Updates ChatGPT Special
    Menu Updates ChatGPT Special

---
+++ menu-data.js
@@ -0,0 +1,19 @@
+window.DC_MENU = [
+  {category:'Donuts', items:[
+    ['Dozen',13.49],['Eight Pack',9.50],['Half Dozen',7.00],['Single',1.50],['Homecut'],['Peanut'],['German Chocolate'],['Dutch Crumb'],['Toasted Coconut'],['Chocolate Iced Ring'],['Vanilla Iced Ring'],['Maple Iced Ring'],['Peanut Butter Iced Ring'],['Honey Dip'],['Chocolate Iced Cake'],['Vanilla Iced Chocolate Cake'],['Double Chocolate'],['Chocolate'],['French Cruller'],['Blueberry Cake'],['Cherry Cake'],['Honey Dew'],['Chocolate Chip'],['Sour Cream'],['Vanilla Angel'],['Strawberry Shortcake'],['Charleston Delight'],['Bavarian Cream'],['Boston Cream'],['Graham Cracker'],['Swiss Chocolate'],['Double Chocolate Angel'],['Peanut Butter Cup'],['Jelly'],['Lemon'],['Dutch Apple'],['Powdered'],['Cinnamon'],['Seasonal Donut']
+  ]},
+  {category:'Fancy Donuts & Pastries', items:[
+    ['Eclair',2.39],['Vanilla Eclair'],['Chocolate Eclair'],['Bavarian Eclair'],['Lady Finger',2.39],['Vanilla Lady Finger'],['Chocolate Lady Finger'],['Bavarian Lady Finger'],['Hot Dog',2.39],['Maple Bacon Long John',2.39],['Brownie',1.59],['Brownie Cream Finger',1.69],['Cream Horn',1.49],['Turnover',1.49],['Cherry Turnover'],['Blueberry Turnover'],['Apple Turnover'],['Pumpkin Turnover (Seasonal)'],['Muffin',1.79],['Banana Nut Muffin'],['Cranberry Orange Muffin'],['Blueberry Muffin'],['Pumpkin Cream Cheese Muffin (Seasonal)'],['Cream-Filled Cookie',2.59],['Chocolate Chip Cookie'],['Double Chocolate Cookie'],['Oatmeal Raisin Cookie'],['Sugar Cookie'],['Fritter',2.39],['Cinnamon Roll',2.39],['Cinnamon Twist',1.59]
+  ]},
+  {category:'Donut Holes', items:[['Single',0.27],['Half Dozen',1.79],['Dozen',3.69],['Box of 30',7.69],['Box of 45',9.29]]},
+  {category:'Cookies', items:[['Dozen',7.29],['Half Dozen',4.19],['Single',0.75],['Chocolate Chip'],['Sugar'],['Double Chocolate'],['Oatmeal Raisin'],['Peanut Butter']]},
+  {category:'Ice Cream', items:[['Donut À La Mode',2.49],['Brownie À La Mode',3.09],['Quart',4.99],['Pint',3.99],['2-Scoop Sundae',3.49],['3-Scoop Sundae',4.49],['Waffle Cone',0.40],['Kids Cone',1.19],['Single Dip',2.49],['Double Dip',3.49]]},
+  {category:'Coffee', items:[['Small',1.99],['Medium',1.69],['Large',1.99],['Extra Large',2.29],['Carafe',16.99],['Travel Mug Refill',0.62],['1 lb. Coffee Beans',7.99]]},
+  {category:'Cappuccinos', items:[['Small',1.99],['Medium',2.29],['Large',2.59],['Extra Large',2.89],['French Vanilla'],['Salted Caramel'],['Pumpkin (Seasonal)']]},
+  {category:'Hot Chocolate', items:[['Small',1.99],['Medium',2.29],['Large',2.59],['Extra Large',2.89],['Carafe',18.99]]},
+  {category:'Cold Brew', items:[['16 oz.',2.19],['20 oz.',2.39],['French Vanilla'],['Salted Caramel'],['Mocha']]},
+  {category:'Bagels', items:[['Everything'],['Blueberry'],['Plain'],['Cinnamon Raisin']]},
+  {category:'Grandwiches', items:[['Pepperoni',3.99],['Ham & Cheese',3.99],['Bacon & Cheese',3.99],['Sausage & Cheese',3.99],['Bacon, Egg & Cheese',4.59],['Sausage, Egg & Cheese',4.59],['Ham, Egg & Cheese',4.59]]},
+  {category:'Milkshakes', items:[['16 oz.',2.79],['20 oz.',3.39]]},
+  {category:'Seasonal & Special Events', items:[['Father\'s Day'],['Valentine\'s Day'],["St. Patrick's Day"],['Fourth of July'],['Easter'],['Baby Shower'],['Gender Reveal'],['Office Party'],['Wedding'],['Birthday']]}
+];
